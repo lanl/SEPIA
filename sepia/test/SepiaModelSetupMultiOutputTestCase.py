06296ea21fe7650efac648ac6b28b009c8f5cdd3
Natalie Elizabeth Klein <neklein@lanl.gov>
2020-06-30 09:59:05 -0600

    add test case with discrep

--- sepia/test/SepiaModelSetupMultiOutputTestCase.py
+++ sepia/test/SepiaModelSetupMultiOutputTestCase.py
@@ -77,9 +77,9 @@ class SepiaModelSetupMultiOutputTestCase(unittest.TestCase):
         self.assertTrue(set(mcmc_list_names) == set(['betaU', 'lamUz', 'lamWOs', 'lamWs']))
 
 
-    def test_multivariate_sim_and_obs_setup(self):
+    def test_multivariate_sim_and_obs_setup_noD(self):
         """
-        Tests setup for multivariate sim and obs model
+        Tests setup for multivariate sim and obs model with no D
         """
 
         d = SepiaData(t_sim=self.data_dict['t_sim'], y_sim=self.data_dict['y_sim'], y_ind_sim=self.data_dict['y_ind_sim'],
@@ -157,6 +157,105 @@ class SepiaModelSetupMultiOutputTestCase(unittest.TestCase):
 
         mcmc_list_names = [p.name for p in model.params.mcmcList]
         self.assertTrue(set(mcmc_list_names) == set(['betaU', 'lamUz', 'lamWOs', 'lamWs', 'lamOs', 'theta']))
+
+
+    def test_multivariate_sim_and_obs_setup(self):
+        """
+        Tests setup for multivariate sim and obs model with D
+        """
+
+        d = SepiaData(t_sim=self.data_dict['t_sim'], y_sim=self.data_dict['y_sim'], y_ind_sim=self.data_dict['y_ind_sim'],
+                      y_obs=self.data_dict['y_obs'], y_ind_obs=self.data_dict['y_ind_obs'])
+        print('Testing multivariate sim and obs SepiaModelSetup with discrep...', flush=True)
+        print(d, flush=True)
+
+        # Try it without doing standardization/transform/pc basis to be sure it doesn't break
+        model_notrans = setup_model(copy.deepcopy(d))
+
+        # Do explicit transformation
+        d.transform_xt()
+        d.standardize_y()
+        d.create_K_basis(n_pc=5)
+        d.create_D_basis(type='constant')
+        d.create_D_basis(type='linear')
+        custom_D = np.vstack([np.ones(d.obs_data.y.shape[1]), d.obs_data.y_ind])
+        d.create_D_basis(D=custom_D)
+        model = setup_model(d)
+
+        # Check that either way gives same transformation
+        self.assertTrue(np.allclose(model_notrans.data.sim_data.orig_y_mean, model.data.sim_data.orig_y_mean))
+        self.assertTrue(np.allclose(model_notrans.data.sim_data.orig_y_sd, model.data.sim_data.orig_y_sd))
+        self.assertTrue(np.allclose(model_notrans.data.sim_data.y_std, model.data.sim_data.y_std))
+        self.assertTrue(np.allclose(model_notrans.data.sim_data.t_trans, model.data.sim_data.t_trans))
+        self.assertTrue(np.allclose(model_notrans.data.obs_data.orig_y_mean, model.data.obs_data.orig_y_mean))
+        self.assertTrue(np.allclose(model_notrans.data.obs_data.orig_y_sd, model.data.obs_data.orig_y_sd))
+        self.assertTrue(np.allclose(model_notrans.data.obs_data.y_std, model.data.obs_data.y_std))
+
+        # Check model components are set up as expected
+        self.assertTrue(not model.num.scalar_out)
+        self.assertTrue(not model.num.sim_only)
+        self.assertTrue(model.num.m == 100)
+        self.assertTrue(model.num.n == 1)
+        self.assertTrue(model.num.p == 1)
+        self.assertTrue(model.num.q == 3)
+        self.assertTrue(model.num.pu == 5)
+        self.assertTrue(model.num.pv == 2)
+        #self.assertTrue(np.allclose(model.num.w, model.data.sim_data.y_std)) # TODO compute projection
+        #self.assertTrue(np.allclose(model.num.u, model.data.obs_data.y_std)) # TODO compute projection
+        # self.assertTrue(np.allclose(model.num.v, model.data.obs_data.y_std)) # TODO compute projection
+
+        # Check parameter setup -- betaU
+        betaU = model.params.betaU
+        self.assertTrue(betaU.val_shape == (model.num.q + model.num.p, model.num.pu))
+        self.assertTrue(betaU.prior.dist == 'Beta')
+        self.assertTrue(betaU.mcmc.stepType == 'BetaRho')
+
+        # Check parameter setup -- betaV
+        betaV = model.params.betaV
+        self.assertTrue(betaV.val_shape == (1, 1))
+        self.assertTrue(betaV.prior.dist == 'Beta')
+        self.assertTrue(betaV.mcmc.stepType == 'BetaRho')
+
+        # Check parameter setup -- lamUz
+        lamUz = model.params.lamUz
+        self.assertTrue(lamUz.val_shape == (1, model.num.pu))
+        self.assertTrue(lamUz.prior.dist == 'Gamma')
+        self.assertTrue(lamUz.mcmc.stepType == 'PropMH')
+
+        # Check parameter setup -- lamUz
+        lamVz = model.params.lamVz
+        self.assertTrue(lamVz.val_shape == (1, 1))
+        self.assertTrue(lamVz.prior.dist == 'Gamma')
+        self.assertTrue(lamVz.mcmc.stepType == 'PropMH')
+
+        # Check parameter setup -- lamWOs
+        lamWOs = model.params.lamWOs
+        self.assertTrue(lamWOs.val_shape == (1, 1))
+        self.assertTrue(lamWOs.prior.dist == 'Gamma')
+        self.assertTrue(lamWOs.mcmc.stepType == 'PropMH')
+
+        # Check parameter setup -- lamWs
+        lamWs = model.params.lamWs
+        self.assertTrue(lamWs.val_shape == (1, model.num.pu))
+        self.assertTrue(lamWs.prior.dist == 'Gamma')
+        self.assertTrue(lamWs.mcmc.stepType == 'PropMH')
+
+        # Check parameter setup -- lamOs
+        lamOs = model.params.lamOs
+        self.assertTrue(lamOs.val_shape == (1, 1))
+        self.assertTrue(lamOs.prior.dist == 'Gamma')
+        self.assertTrue(lamOs.mcmc.stepType == 'PropMH')
+
+        # Check parameter setup -- theta
+        theta = model.params.theta
+        self.assertTrue(theta.val_shape == (1, model.num.q))
+        self.assertTrue(theta.prior.dist == 'Normal')
+        self.assertTrue(theta.mcmc.stepType == 'Uniform')
+        self.assertTrue(np.allclose(theta.orig_range[0], 0))
+        self.assertTrue(np.allclose(theta.orig_range[1], 1))
+
+        mcmc_list_names = [p.name for p in model.params.mcmcList]
+        self.assertTrue(set(mcmc_list_names) == set(['betaU', 'betaV', 'lamUz', 'lamVz', 'lamWOs', 'lamWs', 'lamOs', 'theta']))
 
 
 
