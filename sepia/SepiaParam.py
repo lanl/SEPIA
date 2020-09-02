ec986d17c12c7b13de5374254f31f9e9848b0973
Natalie Elizabeth Klein <neklein@lanl.gov>
2020-09-02 15:38:27 -0600

    update docstrings

--- sepia/SepiaParam.py
+++ sepia/SepiaParam.py
@@ -26,33 +26,32 @@ class SepiaParamList:
 class SepiaParam:
     """
     SepiaParam class contains current value, shape, name, and prior for a single matrix-valued parameter in the model.
-    The attributes of a SepiaParam instance:
 
-    :var val: nparray -- current value
-    :var val_shape: tuple -- shape of current value
-    :var fixed: boolean nparray -- whether certain elements of the parameter should be fixed (will not be sampled)
-    :var name: string -- name of parameter (eg 'betaU', 'theta')
-    :var refVal: nparray -- reference value, used during MCMC to store previous value in case proposal is rejected
-    :var prior: SepiaPrior object -- encapsulates prior for this parameter
-    :var mcmc: SepiaMCMC object -- encapsulates MCMC information and draws for this parameter
-    :var orig_range: list -- range for untransformed parameter, if transformation was applied (typically for 'theta' only)
+    :var numpy.ndarray val: current parameter value
+    :var tuple val_shape: shape of parameter value array
+    :var numpy.ndarray(dtype=bool) fixed: whether certain elements of the parameter should be fixed (will not be sampled)
+    :var string name: name of parameter (eg 'betaU', 'theta')
+    :var numpy.ndarray refVal: reference value, used during MCMC to store previous value in case proposal is rejected
+    :var sepia.SepiaPrior prior: encapsulates prior information for this parameter
+    :var sepia.SepiaMCMC mcmc: encapsulates MCMC information and draws for this parameter
+    :var list orig_range: range for untransformed parameter, if transformation was applied (currently applies only to 'theta')
     """
 
-    def __init__(self, val, name, val_shape=1, dist='Normal', params=[], bounds=None, mcmcStepType='Normal',
+    def __init__(self, val, name, val_shape=1, dist='Normal', params=None, bounds=None, mcmcStepType='Uniform',
                  mcmcStepParam=0.1, orig_range=None):
         """
         Initialize SepiaParam object, which initializes the contained SepiaPrior and SepiaMCMC objects as well.
 
-        :param val: ndarray -- starting value (can be scalar and will then be expanded to val_shape)
-        :param name: string -- parameter name
-        :param val_shape: int, tuple -- shape of parameter
-        :param dist: string -- prior distribution name ('Normal', 'Gamma', 'Beta', 'Uniform')
-        :param params: list -- list of ndarrays, params for prior
-        :param bounds: list -- bounds for parameter values (can be inf)
-        :param mcmcStepType: string -- step type for MCMC ('Normal', 'Uniform', 'PropMH')
-        :param mcmcStepParam: ndarray, scalar -- step size parameter (has val_shape or scalar is expanded to that shape)
-        :param orig_range: list, size 2 -- range for untransformed parameter (applicable for theta)
-        :raises: ValueError if non-scalar val doesn't match val_shape
+        :param np.ndarray/float val: starting value (if scalar, will be expanded to val_shape array)
+        :param string name: parameter name
+        :param tuple val_shape: shape of parameter array
+        :param string dist: prior distribution name in ['Normal', 'Gamma', 'Beta', 'Uniform']
+        :param list/NoneType params: list of ndarrays, params for prior, or None to use default parameters
+        :param list/NoneType bounds: list of bounds for parameter values (can be inf), or None to use default bounds
+        :param string mcmcStepType: step type for MCMC (eg 'Uniform', 'BetaRho', 'PropMH')
+        :param numpy.ndarray/float mcmcStepParam: step size parameter (has val_shape or scalar is expanded to that shape)
+        :param list orig_range: range for untransformed parameter (applicable for theta)
+        :raises ValueError: if non-scalar val doesn't match val_shape
         """
         if np.isscalar(val):
             self.val = val * np.ones(val_shape)
@@ -74,17 +73,21 @@ class SepiaParam:
         self.orig_range = orig_range
 
     def get_num_samples(self):
+        """
+        Return number of samples in self.mcmc.draws.
+
+        """
         return len(self.mcmc.draws)
 
     def mcmc_to_array(self, trim=0, sampleset=False, flat=True, untransform_theta=False):
         """
         Convert internal representation of MCMC draws to an array.
 
-        :param trim: number of samples to trim from beginning of chain
-        :param sampleset: indices of samples to use
-        :param flat: whether to flatten size of parameter
-        :param untransform_theta: Boolean -- whether or not to untransform theta to original scale
-        :return: ndarray -- MCMC draws
+        :param int trim: number of samples to trim from beginning of chain
+        :param list sampleset: indices of samples to use
+        :param bool flat: flatten array shape of parameter?
+        :param bool untransform_theta: untransform theta to original scale? (no effect on other parameters)
+        :return: MCMC draws as np.ndarray
         """
         if type(sampleset) is int: sampleset=[sampleset] # it has to be a list
         if sampleset is not False:
@@ -95,12 +98,17 @@ class SepiaParam:
             draws = self.untransform_theta(draws)
         if flat:
             draws_flat = np.zeros((draws.shape[0], draws.shape[1]*draws.shape[2]))
-            for samp in range(draws.shape[0]): # TODO I don't think a loop is needed here... just one reshape
+            for samp in range(draws.shape[0]):
                 draws_flat[samp, :] = np.ndarray.flatten(draws[samp, :, :], order='F')
             draws = draws_flat
         return draws
 
     def calc_accept_rate(self):
+        """
+        Calculate acceptance rate.
+
+        :return: numpy.ndarray of acceptance rates for each index of the parameter
+        """
         tlist=self.calc_accept()
         return np.sum(tlist,axis=0) / tlist.shape[0]
 
@@ -108,7 +116,7 @@ class SepiaParam:
         """
         Calculate whether each draw was rejected or accepted.
 
-        :return: ndarray -- 0/1 whether accepted or not
+        :return: numpy.ndarray -- 0/1 whether accepted or not
         """
         # Calculate binary accept/reject arrays for each param from mcmc chain
         draws = np.array(self.mcmc.draws)
@@ -123,7 +131,7 @@ class SepiaParam:
         """
         Sets val to new value, expanding to val_shape from scalar if needed.
 
-        :param sval: ndarray, scalar -- new value
+        :param numpy.ndarray/float sval: new value
         """
         if np.isscalar(sval):
             sval = np.array(sval).reshape(self.val_shape)
@@ -133,7 +141,7 @@ class SepiaParam:
         """
         Untransforms an array of theta values using stored t_min and t_max.
 
-        :param sample_array: ndarray -- array of theta samples, (nsamp, theta_size)
+        :param numpy.ndarray sample_array: array of theta samples, (nsamp, theta_size)
         :return: untransformed sample array
         """
         if self.orig_range is not None:
