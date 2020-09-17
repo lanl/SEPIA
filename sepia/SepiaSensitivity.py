ca5879493e063fd22ae4800173bb6f657d5e2252
Natalie Elizabeth Klein <neklein@lanl.gov>
2020-09-17 16:15:27 -0600

    fix bugs jelist/varlist, add tests

--- sepia/SepiaSensitivity.py
+++ sepia/SepiaSensitivity.py
@@ -15,7 +15,7 @@ def sensitivity(model, samples_dict=None, ngrid=21, varlist=None, jelist=None, r
     :param list/string/NoneType varlist: list of tuples giving pairs of variables for which joint effects are desired;
                                          using 'all' indicates to compute joint effects for all variables. Default is None.
     :param list jelist: list of tuples indicating variables for which joint sensitivities are desired (similar to varlist; default is None)
-    :param numpy.ndarray/NoneType rg: matrix with one row for each varaible giving min/max values for sensitivity calculations
+    :param numpy.ndarray/NoneType rg: matrix with one row for each variable giving min/max values for sensitivity calculations
                              assuming unit hypercube scaling, shape (num_vars, 2), default: unit hypercube.
     :param string/dict option: do calculations based on 'mean' (posterior mean GP params), 'median' (posterior median GP params),
                                'samples' (GP param samples in samples_dict), or pass dict of samples from model.get_samples(flat=True)
@@ -96,12 +96,13 @@ def sensitivity(model, samples_dict=None, ngrid=21, varlist=None, jelist=None, r
     sim_xt = model.data.zt
     w = model.num.w.reshape((m, pu), order='F')
     sa = []
+    cat_ind = np.concatenate([model.data.x_cat_ind, model.data.t_cat_ind])
     for ii in range(pu):
         bind = [ind + ii*nv for ind in ii0]
         betaU_sub = betaU[:, bind]
         lamUz_sub = lamUz[:, ii]
         lamWs_sub = lamWs[:, ii]
-        sa.append(component_sens(sim_xt[:, ii0], w[:, ii], betaU_sub, lamUz_sub, lamWs_sub, xe, ngrid, varlist, jelist, rg))
+        sa.append(component_sens(sim_xt[:, ii0], w[:, ii], betaU_sub, lamUz_sub, lamWs_sub, xe, ngrid, varlist, jelist, rg, cat_ind))
 
     # Extract y info from model
     ymean = model.data.sim_data.orig_y_mean
@@ -143,7 +144,7 @@ def sensitivity(model, samples_dict=None, ngrid=21, varlist=None, jelist=None, r
 
     # If jelist is not None, compute sje/sjePm
     if jelist is not None:
-        sje = np.zeros(npvec, len(jelist))
+        sje = np.zeros((npvec, len(jelist)))
         for ii in range(npvec):
             for jj in range(pu):
                 sje[ii, :] += lam[jj] * sa[jj]['sje'][ii, :] * sa[jj]['vt'][ii]
@@ -192,27 +193,28 @@ def sensitivity(model, samples_dict=None, ngrid=21, varlist=None, jelist=None, r
         ysdmat = np.tile(ysd, (ngrid, ngrid))
         for jj in range(pu):
             for kk in range(len(varlist)):
-                jef_m[jj, kk, :, :] = np.kron(np.mean(sa[jj]['jef_m'][:, kk, :, :],0).reshape((1, ngrid)), ksmm[:, jj]) * ysdmat + meanmat
-                jef_sd[jj, kk, :, :] = np.sqrt(np.kron(np.var(sa[jj]['jef_m'][:, kk, :, :], axis=0).reshape((1, ngrid)), ksmm[:, jj]**2) +
-                                               np.kron(np.mean(sa[jj]['jef_v'][:, kk, :, :], axis=0).reshape((1, ngrid)), ksmm[:, jj]**2)) * ysdmat
+                jef_m[jj, kk, :, :] = np.transpose(np.kron(np.mean(sa[jj]['jef_m'][:, kk, :, :], 0).reshape((-1, ngrid)), ksmm[:, jj]) * ysdmat + meanmat)
+                jef_sd[jj, kk, :, :] = np.transpose(np.sqrt(np.kron(np.var(sa[jj]['jef_m'][:, kk, :, :], axis=0).reshape((-1, ngrid)), ksmm[:, jj]**2) +
+                                                    np.kron(np.mean(sa[jj]['jef_v'][:, kk, :, :], axis=0).reshape((-1, ngrid)), ksmm[:, jj]**2)) * ysdmat)
         a = jef_m.shape
         tjef_m = np.sum(jef_m, 0).reshape(a[1:4])
         for kk in range(len(varlist)):
-            tjef_m[kk, :, :] = tjef_m[kk, :, :].reshape(a[2:4]) - (pu-1)*meanmat
-        tjef_m.sqeeze()
+            tjef_m[kk, :, :] = tjef_m[kk, :, :].reshape(a[2:4]) - (pu-1)*meanmat.T
+        tjef_m.squeeze()
         tjef_sd = np.zeros((a[1], a[2], a[3]))
         for jj in range(len(varlist)):
             for kk in range(pu):
-                tjef_sd[jj, :, :]=tjef_sd[jj, :, :].reshape((1, ngrid)) + np.kron(np.mean(sa[kk]['jef_v'][:, jj, :, :], axis=0).reshape((1, ngrid)), ksmm[:, kk]**2)
-        tmp = np.zeros((npvec, a[1], a[2]))
+                tjef_sd[jj, :, :] = tjef_sd[jj, :, :].reshape((-1, ngrid)) + \
+                                    np.kron(np.mean(sa[kk]['jef_v'][:, jj, :, :], axis=0).reshape((-1, ngrid)), ksmm[:, kk]**2).T
+        tmp = np.zeros((npvec, a[1], a[3]))
         for hh in range(ngrid):
             for ii in range(ksmm.shape[0]):
                 for jj in range(len(varlist)):
                     for kk in range(pu):
-                        tmp[:, jj, :] = tmp[:, jj, :].reshape((1, ngrid)) + ksmm[ii, kk] * sa[kk]['jef_m'][:, jj, hh, :].reshape((1, ngrid))
+                        tmp[:, jj, :] = tmp[:, jj, :].reshape((-1, ngrid)) + ksmm[ii, kk] * sa[kk]['jef_m'][:, jj, hh, :].reshape((-1, ngrid))
                     tjef_sd[jj, (hh-1)*ksmm.shape[0]+ii, :] += np.var(tmp[:, jj, :], axis=0)
         for kk in range(len(varlist)):
-            tjef_sd[kk, :, :] = np.sqrt(tjef_sd[kk, :, :].reshape((a[2], a[3])) * ysdmat)
+            tjef_sd[kk, :, :] = np.sqrt(tjef_sd[kk, :, :].reshape((a[2], a[3])) * ysdmat.T)
         tjef_sd.squeeze()
     
     sens = {'sa':sa,
@@ -235,28 +237,25 @@ def sensitivity(model, samples_dict=None, ngrid=21, varlist=None, jelist=None, r
         sens['sjePm'] = sjePm
     return sens
             
-def component_sens(x, y, beta, lamUz, lamWs, xe, ngrid, varlist, jelist, rg):
+def component_sens(x, y, beta, lamUz, lamWs, xe, ngrid, varlist, jelist, rg, cat_ind):
 
     diff = rg[:, 1] - rg[:, 0]
     nmcmc, p = beta.shape
     m = x.shape[0]
 
-    # TODO skipping loading catInd from model for now since not part of model
-    cat_ind = np.zeros(p)
-
     # Calculate x distances
-    xdist = SepiaDistCov(x)
+    xdist = SepiaDistCov(x, cat_ind=cat_ind)
     xexdist = []
     xedist = []
     for ii in range(p):
-        xexdist.append(SepiaDistCov(xe[:, ii][:, None], x[:, ii][:, None]))
-        xedist.append(SepiaDistCov(xe[:, ii][:, None]))
+        xexdist.append(SepiaDistCov(xe[:, ii][:, None], x[:, ii][:, None], cat_ind=cat_ind[[ii]]))
+        xedist.append(SepiaDistCov(xe[:, ii][:, None], cat_ind=cat_ind[[ii]]))
 
     if varlist is not None:
         for ii in range(len(varlist)):
             xte = np.array([(vi, vj) for vi in xe[:, varlist[ii][0]] for vj in xe[:, varlist[ii][1]]])
-            xexdist.append(SepiaDistCov(xte, x[:, varlist[ii]]))
-            xedist.append(SepiaDistCov(xte))
+            xexdist.append(SepiaDistCov(xte, x[:, varlist[ii]], cat_ind=cat_ind[np.array(varlist[ii])]))
+            xedist.append(SepiaDistCov(xte, cat_ind=cat_ind[np.array(varlist[ii])]))
 
     # Calculate GP stuff
     P = np.zeros((nmcmc, m, m))
@@ -269,7 +268,6 @@ def component_sens(x, y, beta, lamUz, lamWs, xe, ngrid, varlist, jelist, rg):
 
         # eta cov for the data & prediction locations
         S = xdist.compute_cov_mat(betaei, lamUzi, lamWsi)
-
         P[ii, :, :] = np.linalg.inv(S)
         My[:, ii] = np.linalg.solve(S, y)
         Q[ii, :, :] = P[ii, :, :] - np.outer(My[:, ii], My[:, ii])
@@ -282,8 +280,7 @@ def component_sens(x, y, beta, lamUz, lamWs, xe, ngrid, varlist, jelist, rg):
     ste = np.zeros((nmcmc, p))
     mef_m = np.zeros((nmcmc, p, ngrid))
     mef_v = np.zeros((nmcmc, p, ngrid))
-    u1 = np.zeros(m)
-    u2 = np.zeros(m)
+
     if varlist is not None:
         sie = np.zeros((nmcmc, len(varlist)))
         jef_m = np.zeros((nmcmc, len(varlist), ngrid, ngrid))
@@ -304,65 +301,65 @@ def component_sens(x, y, beta, lamUz, lamWs, xe, ngrid, varlist, jelist, rg):
         # initial calculations
         c1 = calc1(betaei, diff)
         C2 = calc2(x, xdist, m, rg, betaei, diff)
-        c3 = np.zeros((m,diff.shape[0]))
+        c3 = np.zeros((m, diff.shape[0]))
         for jj in range(m):
-            c3[jj,:]=calc3(x[jj,:],rg,betaei,diff)
-        u2=np.prod(c3,1)
-        e2[ii]=np.prod(c1)/lamUzi-np.trace(np.squeeze(Q[ii,:,:]) @ \
-                                         varf(m,p,[],C2,u2))/lamUzi**2
-        e0[ii]=np.matmul(u2.T,My[:,ii])/lamUzi
+            c3[jj, :] = calc3(x[jj, :], rg, betaei, diff)
+        u2 = np.prod(c3, 1)
+        e2[ii] = np.prod(c1)/lamUzi-np.trace(np.squeeze(Q[ii, :, :]) @ varf(m, p, [], C2, u2))/lamUzi**2
+        e0[ii] = np.matmul(u2.T, My[:, ii])/lamUzi
         # total variance
-        vt[ii]=1/lamUzi-np.trace(np.squeeze(Q[ii,:,:]) @ \
-                                varf(m,p,np.arange(p),C2,[]))/lamUzi**2-e2[ii]
+        vt[ii] = 1/lamUzi - np.trace(np.squeeze(Q[ii, :, :]) @ varf(m, p, np.arange(p), C2, []))/lamUzi**2 - e2[ii]
         # 1:p might be an index so we need an arrange from 0 to p-1
         # main/total effect indices; main effect functions
         for jj in range(p):
-            Js=[jj]; ll=np.setxor1d(np.arange(p),Js)
-            u1=np.prod(c3[:,ll],1); u4=np.prod(c1[ll])
-            sme[ii,jj]=u4/lamUzi-np.trace(np.squeeze(Q[ii,:,:]) @ \
-                                         varf(m,p,Js,C2,u1))/lamUzi**2-e2[ii]
-            sme[ii,jj]=sme[ii,jj]/vt[ii]
-            ME=etae(Js,x,u1,u4,xexdist[jj],xedist[jj],betaei,lamUzi,\
-                   lamWsi,My[:,ii],np.squeeze(P[ii,:,:]))
-            mef_m[ii,jj,:]=ME.m; mef_v[ii,jj,:]=ME.v
-            ll=[jj]; Js=np.setxor1d(np.arange(p),ll); u2=np.prod(c3[:,ll],1)
-            ste[ii,jj]=c1[ll]/lamUzi-np.trace(np.squeeze(Q[ii,:,:]) @ \
-                                              varf(m,p,Js,C2,u2))/lamUzi**2-e2[ii]
-            ste[ii,jj]=1-ste[ii,jj]/vt[ii]
+            Js = [jj]
+            ll = np.setxor1d(np.arange(p), Js)
+            u1 = np.prod(c3[:, ll], 1)
+            u4 = np.prod(c1[ll])
+            sme[ii, jj] = u4/lamUzi - np.trace(np.squeeze(Q[ii, :, :]) @ varf(m, p, Js, C2, u1))/lamUzi**2 - e2[ii]
+            sme[ii, jj] = sme[ii, jj]/vt[ii]
+            ME = etae(Js, u1, u4, xexdist[jj], xedist[jj], betaei, lamUzi, lamWsi, My[:, ii], np.squeeze(P[ii, :, :]))
+            mef_m[ii, jj, :] = ME.m
+            mef_v[ii, jj, :] = ME.v
+            ll = [jj]
+            Js = np.setxor1d(np.arange(p), ll)
+            u2 = np.prod(c3[:, ll], 1)
+            ste[ii, jj] = c1[ll]/lamUzi - np.trace(np.squeeze(Q[ii, :, :]) @ varf(m, p, Js, C2, u2))/lamUzi**2 - e2[ii]
+            ste[ii, jj] = 1 - ste[ii, jj]/vt[ii]
         # two-factor interaction indices, joint effects
         if varlist is not None:
             for jj in range(len(varlist)):
-                Js=varlist[jj,:]; ll=np.setxor1d(np.arange(p),Js)
-                u3=np.prod(c3[:,ll],1); u5=np.prod(c1[ll])
-                sie[ii,jj]=u5/lamUzi-np.trace(np.squeeze(Q[ii,:,:])*\
-                                              varf(m,p,Js,C2,u3))/lamUzi**2-e2[ii]
-                sie[ii,jj]=sie[ii,jj]/vt[ii]-sme[ii,varlist[jj,1]]-\
-                     sme[ii,varlist[jj,2]]
-                JE=etae(Js,x,u3,u5,xexdist[p+jj],xedist[p+jj],betaei,lamUzi,\
-                       lamWsi,My[:,ii],np.squeeze(P[ii,:,:]))
-                jef_m[ii,jj,:,:]=np.reshape(JE.m,(ngrid,ngrid))
-                jef_v[ii,jj,:,:]=np.reshape(JE.v,(ngrid,ngrid))
+                Js = varlist[jj]
+                ll = np.setxor1d(np.arange(p), Js)
+                u3 = np.prod(c3[:, ll], 1)
+                u5 = np.prod(c1[ll])
+                sie[ii, jj] = u5/lamUzi - np.trace(np.squeeze(Q[ii, :, :]) * varf(m, p, Js, C2, u3))/lamUzi**2 - e2[ii]
+                sie[ii, jj] = sie[ii, jj]/vt[ii] - sme[ii, varlist[jj][0]] - sme[ii, varlist[jj][1]]
+                JE = etae(Js, u3, u5, xexdist[p+jj], xedist[p+jj], betaei, lamUzi, lamWsi, My[:, ii], np.squeeze(P[ii, :, :]))
+                jef_m[ii, jj, :, :] = np.reshape(JE.m, (ngrid, ngrid))
+                jef_v[ii, jj, :, :] = np.reshape(JE.v, (ngrid, ngrid))
         # joint effect indices
         if jelist is not None:
             for jj in range(len(jelist)):
-                Js=jelist[jj]; ll=np.setxor1d(np.arange(p),Js)
-                u6=np.prod(c3[:,ll],1); u7=np.prod(c1[ll])
-                sje[ii,jj]=u7/lamUzi-np.trace(np.squeeze(Q[ii,:,:])*\
-                                             varf(m,p,Js,C2,u6))/lamUzi**2-e2[ii]
-                sje[ii,jj]=sje[ii,jj]/vt[ii]
+                Js = jelist[jj]
+                ll = np.setxor1d(np.arange(p), Js)
+                u6 = np.prod(c3[:, ll], 1)
+                u7 = np.prod(c1[ll])
+                sje[ii, jj] = u7/lamUzi - np.trace(np.squeeze(Q[ii, :, :]) * varf(m, p, Js, C2, u6))/lamUzi**2 - e2[ii]
+                sje[ii, jj] = sje[ii, jj]/vt[ii]
                 
-    sa = {'e0':e0,
-          'vt':vt,
-          'sme':sme,
-          'ste':ste,
-          'mef_m':mef_m,
-          'mef_v':mef_v}
+    sa = {'e0': e0,
+          'vt': vt,
+          'sme': sme,
+          'ste': ste,
+          'mef_m': mef_m,
+          'mef_v': mef_v}
     if varlist:
-        sa['sie']=sie
-        sa['jef_m']=jef_m
-        sa['jef_v']=jef_v
+        sa['sie'] = sie
+        sa['jef_m'] = jef_m
+        sa['jef_v'] = jef_v
     if jelist:
-        sa['sje']=sje
+        sa['sje'] = sje
     return sa
         
 def calc1(beta, diff):
@@ -376,7 +373,7 @@ def calc2(x ,xdist, m, rg, beta, diff):
     C2 = np.zeros((int(m*(m+1)/2) + m, beta.shape[0]))
     for ii in range(m):
         for jj in range(ii+1, m):
-            mp = (x[ii,:] + x[jj,:])/2
+            mp = (x[ii, :] + x[jj, :])/2
             di = np.where(np.logical_and(xdist.ind[0]==ii, xdist.ind[1]==jj))
             C2[kk, :] = calc3(mp, rg, 2*beta, diff) * np.exp(-beta * xdist.sqdist[di, :]/2)
             kk += 1
@@ -392,32 +389,36 @@ def calc3(x, rg, beta, diff):
     return c3
 
 def varf(m,p,Js,C2,ef):
-    kk=0; ll=np.setxor1d(np.arange(p),Js); Vf=np.zeros((m,m));
+    kk = 0
+    ll = np.setxor1d(np.arange(p), Js)
+    Vf = np.zeros((m, m))
     for ii in range(m-1):
-        for jj in range(ii+1,m):
-            Vf[ii,jj]=1
-            if len(Js) != 0: Vf[ii,jj]=np.prod(C2[kk,Js])
-            if len(ll) != 0: Vf[ii,jj]=Vf[ii,jj]*ef[ii]*ef[jj]
-            kk = kk + 1;
-    Vf=Vf+Vf.T;
+        for jj in range(ii+1, m):
+            Vf[ii, jj] = 1
+            if len(Js) != 0: Vf[ii, jj] = np.prod(C2[kk, Js])
+            if len(ll) != 0: Vf[ii, jj] = Vf[ii, jj] * ef[ii] * ef[jj]
+            kk += 1
+    Vf = Vf + Vf.T
     for ii in range(m):
-        kk = kk + 1; Vf[ii,ii]=1;
-        if len(Js) != 0: Vf[ii,ii]=np.prod(C2[kk,Js])
-        if len(ll) != 0: Vf[ii,ii]=Vf[ii,ii]*(ef[ii]**2)
+        kk += 1
+        Vf[ii, ii] = 1
+        if len(Js) != 0: Vf[ii, ii] = np.prod(C2[kk, Js])
+        if len(ll) != 0: Vf[ii, ii] = Vf[ii,ii] * (ef[ii]**2)
     return Vf
 
 class ee_struct:
-    def __init__(self,m_dim,v_dim):
+    def __init__(self, m_dim, v_dim):
         self.m = np.zeros(m_dim)
         self.v = np.zeros(v_dim)
-def etae(Js,x,ef,vf,xexdist,xedist,beta,lamUz,lamWs,My,P):
-    nxe=xedist.n
-    ee = ee_struct(m_dim=nxe,v_dim=nxe)
-    Ct = xexdist.compute_cov_mat(beta[Js].T,lamUz)
-    Ct = Ct*np.tile(ef.T,(nxe,1))
-    ee.m=np.matmul(Ct,My)
-    C = xedist.compute_cov_mat(beta[Js].T,lamUz,lamWs)
-    ee.v=np.diag(C*vf-np.matmul(np.matmul(Ct,P),Ct.T))
+
+def etae(Js, ef, vf, xexdist, xedist, beta, lamUz, lamWs, My, P):
+    nxe = xedist.n
+    ee = ee_struct(m_dim=nxe, v_dim=nxe)
+    Ct = xexdist.compute_cov_mat(beta[np.array(Js)].T, lamUz)
+    Ct = Ct * np.tile(ef.T, (nxe, 1))
+    ee.m = np.matmul(Ct, My)
+    C = xedist.compute_cov_mat(beta[np.array(Js)].T, lamUz, lamWs)
+    ee.v = np.diag(C*vf - np.matmul(np.matmul(Ct, P), Ct.T))
     return ee
 
 
--- test/test_SepiaSensitivity.py
+++ test/test_SepiaSensitivity.py
@@ -8,11 +8,9 @@ from sepia.SepiaSensitivity import sensitivity
 
 np.random.seed(42)
 
-# TODO: not done, waiting for Sensitivty to be done
-
 class SepiaSensitivityTestCase(unittest.TestCase):
 
-    def setUp(self, m=100, n=1, nt_sim=50, nt_obs=20, n_theta=3, n_basis=5, sig_n=0.1, seed=42):
+    def setUp(self, m=20, n=1, nt_sim=30, nt_obs=20, n_theta=3, n_basis=5, sig_n=0.1, seed=42):
         multi_data_dict = generate_data.generate_multi_sim_and_obs(m=m, n=n, nt_sim=nt_sim, nt_obs=nt_obs,
                                                                    n_theta=n_theta, n_basis=n_basis,
                                                                    sig_n=sig_n, seed=seed)
@@ -54,17 +52,24 @@ class SepiaSensitivityTestCase(unittest.TestCase):
 
     def test_univariate_sim_only_sensitivity(self):
         """
-        Testssensitivity for univariate sim only model
+        Tests sensitivity for univariate sim only model
         """
 
         print('Testing univariate sim-only SepiaSensitivity...', flush=True)
 
         model = self.univ_sim_only_model
 
-        model.tune_step_sizes(50, 10)
-        model.do_mcmc(100)
+        model.do_mcmc(20)
         samples_dict = model.get_samples(20)
-        #sens = sensitivity(model, samples_dict)
+        sens = sensitivity(model, jelist=[(0, 1)])
+        sens = sensitivity(model, varlist='all')
+        sens = sensitivity(model, samples_dict)
+        sens = sensitivity(model)
+        sens = sensitivity(model, ngrid=10)
+        sens = sensitivity(model, option='median')
+        sens = sensitivity(model, option='samples')
+        sens = sensitivity(model, option=model.get_samples(numsamples=10, flat=True))
+
 
     def test_multivariate_sim_only_sensitivity(self):
         """
@@ -75,10 +80,16 @@ class SepiaSensitivityTestCase(unittest.TestCase):
 
         model = self.multi_sim_only_model
 
-        model.tune_step_sizes(50, 10)
-        model.do_mcmc(100)
+        model.do_mcmc(20)
         samples_dict = model.get_samples(20)
-        #sens = sensitivity(model, samples_dict)
+        sens = sensitivity(model, jelist=[(0, 1), (0, 2)])
+        sens = sensitivity(model, varlist='all')
+        sens = sensitivity(model, samples_dict)
+        sens = sensitivity(model)
+        sens = sensitivity(model, varlist=[(0, 1), (0, 2)])
+        sens = sensitivity(model, option='median')
+        sens = sensitivity(model, option='samples')
+        sens = sensitivity(model, option=model.get_samples(numsamples=10, flat=True))
 
 
 
