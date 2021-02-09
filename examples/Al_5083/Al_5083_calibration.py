da296a5fac59e8ad40d8fcb043f4763b25e0575b
gatt <gatt@lanl.gov>
2021-02-09 12:33:46 -0700

    Update to new method od using observation error

--- examples/Al_5083/Al_5083_calibration.py
+++ examples/Al_5083/Al_5083_calibration.py
@@ -24,17 +24,18 @@ y_obs = np.concatenate([obs_s104[sel_features], obs_s105[sel_features], obs_s106
 n_features = y_obs.shape[1]
 y_ind = np.arange(1, n_features+1)
 
-# Set up sepia model
-data = SepiaData(t_sim=design, y_sim=y_sim, y_ind_sim=y_ind, y_obs=y_obs, y_ind_obs=y_ind)
+# We have a known observation error
+Sigy = np.diag( np.squeeze(0.01 * np.ones(n_features) * y_obs) )
+
+# Set up sepia problem dataset
+data = SepiaData(t_sim=design, y_sim=y_sim, y_ind_sim=y_ind, y_obs=y_obs, y_ind_obs=y_ind, Sigy=Sigy)
 data.standardize_y()
 data.transform_xt()
 data.create_K_basis(n_features - 1)
 print(data)
 
 # Setup model
-# We have a known observation error
-Sigy = np.diag(np.squeeze((0.01 * np.ones(n_features) * y_obs)/data.sim_data.orig_y_sd**2))
-model = SepiaModel(data, Sigy)
+model = SepiaModel(data)
 
 # Modify priors to match Matlab
 model.params.lamWs.prior.bounds[1] = np.inf
@@ -45,7 +46,7 @@ model.tune_step_sizes(100, 25)
 model.do_mcmc(10000)
 samples_dict = model.get_samples()
 
-with open('data/sepia_mcmc_samples1-5000.pkl', 'wb') as f:
+with open('data/sepia_mcmc_samples10000.pkl', 'wb') as f:
     pickle.dump(samples_dict, f)
 
 with open('data/sepia_model.pkl', 'wb') as f:
