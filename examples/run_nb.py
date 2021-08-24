6e3dc975dc33e699171ce8680875baee5cc99b8d
Grant Hutchings <granthutchings@Grants-MacBook-Pro.local>
2021-08-24 13:15:03 -0700

    run all notebooks

--- examples/run_nb.py
+++ examples/run_nb.py
@@ -3,38 +3,72 @@ import sys
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 from nbconvert import HTMLExporter
+import glob
 
-def run_notebook(notebook_filename='',execute_path='.',html=0):
-    # open notebook
-    with open(notebook_filename) as f:
-        nb = nbformat.read(f, as_version=4)
-    # set up execution
-    ep = ExecutePreprocessor(timeout=-1, kernel_name='python3')
-    # run execution
-    print('executing:',notebook_filename)
-    ep.preprocess(nb, {'metadata': {'path': execute_path}})
-    if html:
-        # export to html
+def export_to_html(nb,file):
         html_exporter = HTMLExporter()
         html_exporter.exclude_input = True
         html_data, resources = html_exporter.from_notebook_node(nb)
         # write to output file
-        outfile = os.path.splitext(notebook_filename)[0]+'.html'
+        outfile = os.path.splitext(file)[0]+'.html'
         print('exporting:',outfile)
         with open(outfile, 'w') as f:
             f.write(html_data)
+            
+# search basedir for .ipynb files recursively and run all
+def run_all_notebooks(basedir='.',html=0):
+    # open notebook
+    nb_filenames = glob.glob(basedir + '/**/*.ipynb', recursive=True)
+    print('notebooks found:')
+    for file_name in nb_filenames:
+        print(file_name)
+    
+    print('executing notebooks:')
+    for file_name in nb_filenames:
+        execute_path = os.path.split(file_name)[0] # get execute directory
+                
+        with open(file_name) as f:
+            nb = nbformat.read(f, as_version=4)
+        # set up execution
+        ep = ExecutePreprocessor(timeout=-1, kernel_name='python3')
+        # run execution
+        print(file_name)
+        ep.preprocess(nb, {'metadata': {'path': execute_path}})
+        if html:
+            export_to_html(nb,file_name)
+     
+def run_notebook(file_name='', basedir='.', html=0):
+    """
+    Execute a notebook, or all notebooks in a directory, and optionally export to html
+
+    :var string file_name: file name of notebook to be executed
+    :var string basedir: base directory, or location to execute notebook. Imports may fail if execute path is improperly set
+    :var bool html: flag for html export
+    """
+    if file_name == 'all':
+        run_all_notebooks(basedir,html)
+    else:
+        with open(file_name) as f:
+                nb = nbformat.read(f, as_version=4)
+        # set up execution
+        ep = ExecutePreprocessor(timeout=-1, kernel_name='python3')
+        # run execution
+        print('executing:',file_name)
+        ep.preprocess(nb, {'metadata': {'path': basedir}})
+        if html:
+            export_to_html(nb,file_name)
 
 if __name__ == "__main__":
     if len(sys.argv)>1:
-        notebook_filename = sys.argv[1]
+       	notebook_filename = sys.argv[1]
     else:
-        notebook_filename = ''
+        notebook_filename = 'all'
     if len(sys.argv)>2:
-        execute_path = sys.argv[2]
+        basedir = sys.argv[2]
     else:
-        execute_path = '.'
+        basedir = '.'
     if len(sys.argv)>3:
         html = sys.argv[3]
     else:
         html = 0
-    run_notebook(notebook_filename,execute_path,html)
+    run_notebook(notebook_filename,basedir,html)
