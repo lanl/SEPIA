720d55a424a9ce1d6788fa5d13267ac4fa3ea34f
Arthur Lui <luiarthur@gmail.com>
2021-10-27 08:51:26 -0600

    Removed unused imports from SepiaData.py

--- sepia/SepiaData.py
+++ sepia/SepiaData.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
- 
+
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 
 from sepia.DataContainer import DataContainer
-
-sns.set()
-
 
 class SepiaData(object):
     """
